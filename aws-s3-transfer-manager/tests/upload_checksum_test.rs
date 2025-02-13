46ebb204c80a589c47b8e02549f733fba6238cf8
Waqar Ahmed Khan <waahm7@gmail.com>
2025-02-13 15:29:06 -0800

    remove import

--- aws-s3-transfer-manager/tests/upload_checksum_test.rs
+++ aws-s3-transfer-manager/tests/upload_checksum_test.rs
@@ -20,7 +20,6 @@ use aws_sdk_s3::{
     types::{ChecksumAlgorithm, ChecksumType},
 };
 use aws_smithy_mocks_experimental::{mock, Rule, RuleMode};
-use aws_smithy_runtime::test_util::capture_test_logs::capture_test_logs;
 use bytes::Bytes;
 use pin_project_lite::pin_project;
 use test_common::mock_client_with_stubbed_http_client;
